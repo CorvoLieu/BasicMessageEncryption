ed56a9cf1fbe60559f7bad047a242421913373f1
CorvoLieu <20127492@student.hcmus.edu.vn>
2022-12-20 14:07:57 +0700

    Innit

---
+++ Final Prj/algorithm.h
@@ -0,0 +1,80 @@
+#ifndef ALGORITHM_H
+#define ALGORITHM_H
+
+#include <iostream>
+#include <cstdlib>
+#include <vector>
+#include <cstring>
+#include <stdlib.h>
+#include <Windows.h>
+using namespace std;
+
+class BigInt{
+    string digits;
+public:
+ 
+    //Constructors:
+    BigInt(unsigned long long n = 0);
+    BigInt(string &);
+    BigInt(const char *);
+    BigInt(const BigInt &);
+ 
+    //Helper Functions:
+    friend void divide_by_2(BigInt &a);
+    friend bool Null(const BigInt &);
+    friend int Length(const BigInt &);
+    int operator[](const int)const;
+ 
+    /* * * * Operator Overloading * * * */
+ 
+    //Direct assignment
+    BigInt &operator=(const BigInt &);
+ 
+    //Post/Pre - Incrementation
+    BigInt &operator++();
+    BigInt operator++(int temp);
+    BigInt &operator--();
+    BigInt operator--(int temp);
+ 
+    //Addition and Subtraction
+    friend BigInt &operator+=(BigInt &, const BigInt &);
+    friend BigInt operator+(const BigInt &, const BigInt &);
+    friend BigInt operator-(const BigInt &, const BigInt &);
+    friend BigInt &operator-=(BigInt &, const BigInt &);
+ 
+    //Comparison operators
+    friend bool operator==(const BigInt &, const BigInt &);
+    friend bool operator!=(const BigInt &, const BigInt &);
+ 
+    friend bool operator>(const BigInt &, const BigInt &);
+    friend bool operator>=(const BigInt &, const BigInt &);
+    friend bool operator<(const BigInt &, const BigInt &);
+    friend bool operator<=(const BigInt &, const BigInt &);
+ 
+    //Multiplication and Division
+    friend BigInt &operator*=(BigInt &, const BigInt &);
+    friend BigInt operator*(const BigInt &, const BigInt &);
+    friend BigInt &operator/=(BigInt &, const BigInt &);
+    friend BigInt operator/(const BigInt &, const BigInt &);
+ 
+    //Modulo
+    friend BigInt operator%(const BigInt &, const BigInt &);
+    friend BigInt &operator%=(BigInt &, const BigInt &);
+ 
+    //Power Function
+    friend BigInt &operator^=(BigInt &,const BigInt &);
+    friend BigInt operator^(BigInt &, const BigInt &);
+ 
+    //Read and Write
+    friend ostream &operator<<(ostream &,const BigInt &);
+
+    //Hao
+    static BigInt genRandomNum(int size);
+    static BigInt gcd(BigInt a, BigInt b);
+};
+
+BigInt mulmod(BigInt a, BigInt b, BigInt mod);
+BigInt modulo(BigInt A, BigInt B, BigInt mod);
+bool Miller(BigInt p,int iteration);
+
+#endif
---
+++ Final Prj/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include "dos.h"
+#include "rsa_keygen.h"
+using namespace std;
+
+int main()
+{
+	char choice;
+	cout << ((DEBUG) ? "-------------Key Generator Started-------------\n" : "");
+	cout << ((DEBUG) ? "Select key size:\n1. 512 bits\n2. 1024 bits\n3. 2048 bits\n" : "");
+	cout << ((DEBUG) ? "-----------------\nYou select: " : "");
+
+	cin >> choice;
+	
+	int len = 0;
+
+	if (choice == '1')
+		len = 155;
+
+	if (choice == '2')
+		len = 309;
+
+	if (choice == '3')
+		len = 617;
+
+	RSA* rsa = RSA::getInstance(len);
+	cout << "Public key (E):" << rsa->getE() << "- " << Length(rsa->getE()) << " digits\n";
+	cout << "Public key (N):" << rsa->getN() << "- " << Length(rsa->getN()) << " digits\n";
+	cout << "Private key (D):" << rsa->getD() << "- " << Length(rsa->getD()) << " digits\n";
+
+	return 0;
+}
